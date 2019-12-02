a65440030716058a6a706cda79e7913794642de5
Tim Acker <tjacker31@gmail.com>
2019-12-01 19:47:40 -0500

    Adds simple component showing number of checked in passengers

---
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+	selector: 'passenger-count',
+	template: `
+		<div>
+			<h3>Airline Passengers</h3>
+			<div>Total checked in: {{ checkedInCount() }} of {{ items.length }} passengers</div>
+		</div>
+	`
+})
+export class PassengerCountComponent implements OnInit {
+	@Input() items: Passenger[];
+
+	checkedInCount(): number {
+		if (this.items) {
+			return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+		}
+	}
+
+	ngOnInit() {}
+}
--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 
 @NgModule({
-	declarations: [PassengerDashboardComponent],
+	declarations: [PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent],
 	imports: [CommonModule],
 	exports: [PassengerDashboardComponent]
 })
