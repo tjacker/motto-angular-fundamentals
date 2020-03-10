9a3a832346545dc1ab1a58264e03522e3393cc53
Tim Acker <tjacker31@gmail.com>
2020-03-09 20:06:06 -0400

    Replace children field in passenger data with baggage

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -19,7 +19,6 @@ import { Passenger } from '../../models/passenger.interface';
 					detail.checkInDate ? (detail.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in'
 				}}
 			</div>
-			<div class="children">Children: {{ detail.children?.length || 0 }}</div>
 			<button (click)="toggleEdit()">{{ editing ? 'Save' : 'Edit' }}</button>
 			<button (click)="onRemove()">Remove</button>
 		</div>
--- src/app/passenger-dashboard/models/passenger.interface.ts
+++ src/app/passenger-dashboard/models/passenger.interface.ts
@@ -1,12 +1,7 @@
-export interface Child {
-	name: string;
-	age: number;
-}
-
 export interface Passenger {
 	id: number;
 	fullName: string;
 	checkedIn: boolean;
 	checkInDate: number | null;
-	children: Child[] | null;
+	baggage: string;
 }
